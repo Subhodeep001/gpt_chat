b42b3bb389fca6323024c98134f5ffce1286f80e
Subhodeep001 <subhodeep.chatterjee@sticsoftsolutions.com>
2025-12-09 11:23:27 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 import streamlit as st
+import streamlit.components.v1 as components
 
-st.title("Open ChatGPT From Streamlit")
+st.title("Open ChatGPT from Streamlit")
 
-# JavaScript trick to open new tab
 chat_text = "hi how are you today"
 url = f"https://chat.openai.com/?q={chat_text}"
 
 if st.button("Open ChatGPT Window"):
-    js = f"window.open('{url}', '_blank').focus();"
-    st.markdown(f"<script>{js}</script>", unsafe_allow_html=True)
+    components.html(
+        f"""
+        <script>
+            window.open("{url}", "_blank");
+        </script>
+        """,
+        height=0,
+    )
